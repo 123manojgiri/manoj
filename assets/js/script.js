fbd76d96541f19488ff6cd78ba9497a521366939
123manojgiri <106793118+123manojgiri@users.noreply.github.com>
2023-06-14 17:54:02 +0545

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["graphics designing", "web designing", "logo designing", "web development"],
+    strings: ["graphics designing", "web designing", "logo designing", "web development", "Electronics, Communication And Information Engineering"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
